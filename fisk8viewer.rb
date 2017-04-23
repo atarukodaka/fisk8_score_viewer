de0c68ee25e4803740cfe25a2e607f4f7af87917
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-04-23 13:54:13 +0000

    logger-utils-added

--- fisk8viewer.rb
+++ fisk8viewer.rb
@@ -1,40 +1,12 @@
-require 'open-uri'
-require 'pdftotext'
-
 module Fisk8Viewer
   VERSION = 1.0
-
-  module Logger
-    def logger
-      if @logger.nil?
-        @logger = ::Logger.new(STDERR)
-        @logger.datetime_format = '%Y-%m-%d %H:%M'
-      end
-      @logger
-    end
-  end ## module
-
-  module Utils
-    def convert_pdf(url, dir: "./")
-      return "" if url.blank?
-
-      ## create dir if not exists
-      FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
-
-      ## convert pdf to text
-      filename = File.join(dir, URI.parse(url).path.split('/').last)
-      open(url) do |f|
-        File.open(filename, "w") do |out|
-          out.puts f.read
-        end
-      end
-      Pdftotext.text(filename)
-    end
-  end
 end
 
 ################
 ## require relevant libs
+require 'fisk8viewer/utils'
+require 'fisk8viewer/logger'
+
 require 'fisk8viewer/competition_parser'
 require 'fisk8viewer/skater_parser'
 require 'fisk8viewer/score_parser'
---
+++ fisk8viewer/utils.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+require 'pdftotext'
+
+module Fisk8Viewer
+  module Utils
+    def convert_pdf(url, dir: "./")
+      return "" if url.blank?
+
+      ## create dir if not exists
+      FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
+
+      ## convert pdf to text
+      filename = File.join(dir, URI.parse(url).path.split('/').last)
+      open(url) do |f|
+        File.open(filename, "w") do |out|
+          out.puts f.read
+        end
+      end
+      Pdftotext.text(filename)
+    end
+  end  ## module
+end
